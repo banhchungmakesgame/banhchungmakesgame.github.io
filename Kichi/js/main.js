6429450089e4bf37027706811692782966418fb8
zzvutienhung <hungvt89@gmail.com>
2018-04-10 01:55:56 +0700

    aaa

--- Kichi/js/main.js
+++ Kichi/js/main.js
@@ -1,15 +1,10 @@
-FBInstant.initializeAsync().then(function() {
-    FBInstant.setLoadingProgress(100);
-    FBInstant.startGameAsync().then(function() {
-        
-        var game = new Phaser.Game(480, 800, Phaser.AUTO, '');
+   
+    var game = new Phaser.Game(480, 800, Phaser.AUTO, '');
 
-        game.state.add('Boot', TH.Boot);
-        game.state.add('Preloader', TH.Preloader);
-        game.state.add('MainMenu', TH.MainMenu);
-        game.state.add('Gameplay', TH.Gameplay);
-        game.state.add('Result', TH.Result);
+    game.state.add('Boot', TH.Boot);
+    game.state.add('Preloader', TH.Preloader);
+    game.state.add('MainMenu', TH.MainMenu);
+    game.state.add('Gameplay', TH.Gameplay);
+    game.state.add('Result', TH.Result);
 
-        game.state.start('Boot');
-    })
-})
+    game.state.start('Boot');
