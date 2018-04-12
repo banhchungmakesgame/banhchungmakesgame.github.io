c1e09eb38b01bc789965fe77b149b8303b6170ab
zzvutienhung <hungvt89@gmail.com>
2018-04-12 09:25:31 +0700

    aaaa

--- Kichi/js/mainmenu.js
+++ Kichi/js/mainmenu.js
@@ -32,7 +32,6 @@ TH.MainMenu.prototype =
     }, 
     create: function()
     {         
-        game.state.start('Gameplay');  
         var bg = game.add.image(game.world.centerX, game.world.centerY, 'bg');
         bg.scale.setTo(1, 1);
         bg.anchor.set(0.5);
